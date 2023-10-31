3b7f22250898c7238542b1f36d7c641923ab9f95
Patryk Kudlik <patryk.kudlik@gmail.com>
2023-10-31 20:59:59 +0100

    feat: add astrojs/node adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
+import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: process.env.ASTRO_URL,
   output: "server",
   integrations: [sitemap()],
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
